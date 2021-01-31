948e00f219491d135efa913def921adbc321ecb0
mic <micmor.m@gmail.com>
2021-01-31 17:46:07 -0500

    add state to handle textInput value

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  //manage the state for each key pressed
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  //manage the state when the button is press
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Course Goal" />
-        <Button title="ADD" />
+        <TextInput
+          style={styles.input}
+          placeholder="Course Goal"
+          // only pass the reference  of the function without parentesis
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        {/* whitout parentesis because I dont want the function execute immediatly */}
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View></View>
     </View>
